4572db5c467254cc9c1bb1765fb9f45baa83d326
TopchetoEU <36534413+TopchetoEU@users.noreply.github.com>
2024-01-06 17:49:36 +0200

    feat: some array tests

--- tests/index.js
+++ tests/index.js
@@ -2,19 +2,24 @@ function assert(cond, msg, locDepth) {
     if (locDepth < 0 || locDepth === undefined) locDepth = 0;
     if (!cond) {
         log('Assert failed', (typeof locDepth === 'string' ? locDepth : Error().stack[locDepth + 1]) + ': ', msg);
+        return false;
     }
+    return true;
 }
 function assertMatch(expected, actual, depth, msg) {
     if (!match(expected, actual, depth)) {
         log('Assert failed', Error().stack[1] + ': ', msg);
         log('Expected:', expected);
         log('Actual:', actual);
+        return false;
     }
+    return true;
 }
 function match(expected, actual, depth) {
     if (!Array.isArray(expected) || !Array.isArray(actual)) return expected === actual;
     else if (expected.length !== actual.length) return false;
-    else if (depth === undefined || depth < 0) depth = 0;
+    else if (depth === undefined) depth = Infinity;
+    else if (depth < 0) depth = 0;
 
     for (var i = 0; i < expected.length; i++) {
         if (!(i in expected) || !(i in actual)) return !(i in expected) && !(i in actual);
@@ -43,6 +48,7 @@ UnitTest.prototype.run = function(path) {
     if (path === undefined) path = [];
 
     path.push(this.name);
+    var res = true;
 
     if (typeof this.exec === 'function') {
         var res = true, err = 'exec() returned false.';
@@ -50,13 +56,15 @@ UnitTest.prototype.run = function(path) {
             if (this.exec() === false) res = false;
         }
         catch (e) { res = false; err = e; }
-        assert(res, path.join('/') + ': ' + err, this.exec.location());
+        res &= assert(res, path.join('/') + ': ' + err, this.exec.location());
     }
     for (var i = 0; i < this.subtests.length; i++) {
-        this.subtests[i].run(path);
+        res &= this.subtests[i].run(path);
     }
 
     path.pop();
+
+    return res;
 }
 UnitTest.prototype.add = function(test, exec) {
     if (test instanceof UnitTest) this.subtests.push(test);
@@ -64,7 +72,10 @@ UnitTest.prototype.add = function(test, exec) {
     return this;
 }
 
-include('arithmetics/index.js').run();
-include('array/index.js').run();
-
-log('Tests complete.');
+return function() {
+    if (
+        require('arithmetics/index.js').run() &&
+        require('array/index.js').run()
+    ) log('All tests passed!');
+    exit();
+}
