46136e77e21dd10529101f06eec46fbfbd069539
TopchetoEU <36534413+TopchetoEU@users.noreply.github.com>
2024-01-11 10:47:41 +0200

    build: improve build scripts

--- src/java/me/topchetoeu/jscript/common/Metadata.java
+++ src/java/me/topchetoeu/jscript/common/Metadata.java
@@ -1,9 +1,18 @@
 package me.topchetoeu.jscript.common;
 
+import me.topchetoeu.jscript.common.json.JSON;
+
 public class Metadata {
-    private static final String VERSION = "${VERSION}";
-    private static final String AUTHOR = "${AUTHOR}";
-    private static final String NAME = "${NAME}";
+    private static final String VERSION;
+    private static final String AUTHOR;
+    private static final String NAME;
+
+    static {
+        var data = JSON.parse(null, Reading.resourceToString("metadata.json")).map();
+        VERSION = data.string("version");
+        AUTHOR = data.string("author");
+        NAME = data.string("name");
+    }
 
     public static String version() {
         if (VERSION.equals("$" + "{VERSION}")) return "1337-devel";
