313b20a3b3ecd75afd46bea9e796e594354a32b7
marregui <miguel.arregui@gmail.com>
2024-09-04 14:29:17 +0200

    Add first test (#23)
    * Add the first test and upgrade build.gradle to modern standards
    
    1. gradle wrapper
    2. ./gradlew run
    3. manifest will look like
        Manifest-Version: 1.0
        Main-Class: me.topchetoeu.jscript.runtime.SimpleRepl
        Build-Timestamp: 2024-09-04T10:44:35.990+0200
        Build-Branch: ma/add-first-tests
        Build-Revision: 412edc0ebcf01c67403e446a102cd14871c8f678
        Build-Jdk: 21.0.3 (Oracle Corporation 21.0.3+7-LTS-152)
        Build-Author: TopchetoEU
    4. build/distributions contains a zip and a jar which contain jscript-0.9.41-beta.jar
    5. unnecessary libs have been removed
    6. gradle has been updated to 8.10
    7. first test has been added
    
    * fix: revert removal of Jabel (for support of Java 11)
    
    ---------
    
    Co-authored-by: TopchetoEU <36534413+TopchetoEU@users.noreply.github.com>

---
+++ src/test/java/me/topchetoeu/jscript/TestHelloWorld.java
@@ -0,0 +1,14 @@
+package me.topchetoeu.jscript;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class TestHelloWorld {
+
+    @Test
+    public void testHelloWorld() {
+        final String message = "Hello World!";
+        assertEquals("Hello World!", message);
+    }
+}
